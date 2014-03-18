cb47fb4a296c7fad7be77cab3f208d0399f2dd28
Kirby Turner <kirby@thecave.com>
2014-03-18 11:12:04 -0400

    Change script to include markdown header and footer.
    The header and footer includes instructions, license info, etc.

--- lscs.py
+++ lscs.py
@@ -75,12 +75,75 @@ def showAsMarkdown(filename, dict):
         print '    ' + line + '  '
     print ''
 
+def printMarkdownHeader():
+  print '''# Xcode Code Snippets
+
+This is the collection of my user-defined code snippets for Xcode. Free feel to add them to your own collection.
+
+## Install
+
+* Clone, fork or [download](https://github.com/kirbyt/XcodeCodeSnippets/zipball/master) this collection of code snippets.
+* Copy the code snippet files to: `~/Library/Developer/Xcode/UserData/CodeSnippets`
+
+### Hint
+
+To make life easier for me, I clone this repo to my CodeSnippets directory. This makes it easier for me to keep this repo up to date and simplifies sharing my code snippets across multiple machines. You can do the same, but I recommend forking this repo first so you can store your own code snippets along with the ones in this collection.
+
+# Code Snippet Listing <a id="codesnippetlisting"></a>
+
+Each code snippet shortcut is prefixed with **wps** to avoid conflict with other shortcuts. This also groups the code snippets in the Code Completion window.
+'''
+
+
+def printMarkdownFooter():
+  print '''# lscs.py Script
+
+`lscs.py` is a python script that lists the code snippets for the current user. The listing shows the title, shortcut, and summary for each code snippet file.
+
+    Example: python lscs.py
+
+`lscs.py` is also used to create the list of code snippets shown above. You can do the same by using the `-f markdown` (or `--format=markdown`) command line parameter.
+
+    Example: python lscs.py --f markdown
+
+# Support, Bugs and Feature requests
+
+There is absolutely no support offered for this collection of code snippets. You're on your own! However, free feel to send me a message or even a pull request if you have an interesting and useful code snippet to share.
+
+# License
+
+The MIT License
+
+Copyright (c) 2012-2014 White Peak Software Inc
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.'''
+
+
 def main():
     format = None
     opts, args = getopt.getopt(sys.argv[1:], 'f:', ['format='])
     for o, a in opts:
         if o in ('-f', '--format'):
             format = a
+
+    if format == 'markdown':
+      printMarkdownHeader()
 
     codeSnippets = [];
 
@@ -101,6 +164,9 @@ def main():
           showAsMarkdown(filename, dict)
       else:
           show(filename, dict)
+
+    if format == 'markdown':
+      printMarkdownFooter()
 
 if __name__ == '__main__':
   sys.exit(main())
